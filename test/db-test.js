7c2222af11685d1ba313cf2be583991b3771df7a
miktown <mcacho@mimotic.com>
2016-10-18 20:47:19 +0200

    second commit

---
+++ test/db-test.js
@@ -0,0 +1,136 @@
+'use strict'
+
+const test = require('ava')
+const uuid = require('uuid-base62')
+const r = require('rethinkdb')
+const Db = require('../')
+const utils = require('../lib/utils')
+const fixtures = require('./fixtures')
+
+test.beforeEach('setup database', async t => {
+  const dbName = `mimotic_${uuid.v4()}`
+  const db = new Db({ db: dbName })
+
+  await db.connect()
+
+  t.context.db = db
+  t.context.dbName = dbName
+
+  t.true(db.connected, 'should be connected')
+})
+
+test.afterEach.always('cleanup database', async t => {
+  let db = t.context.db
+  let dbName = t.context.dbName
+
+  await db.disconnect()
+
+  t.false(db.connected, 'should be disconnected')
+
+  let conn = await r.connect({})
+  await r.dbDrop(dbName).run(conn)
+})
+
+test('save image', async t => {
+  let db = t.context.db
+  t.is(typeof db.saveImage, 'function', 'saveImage is function')
+
+  let image = fixtures.getImage()
+
+  let created = await db.saveImage(image)
+  t.is(created.url, image.url, 'retorna correctamente la url')
+  t.is(created.likes, image.likes)
+  t.is(created.liked, image.liked)
+  t.is(created.description, image.description)
+  t.deepEqual(created.tags, [ 'awesome', 'platzi', 'tags' ], 'crea correctamente las tags')
+  t.is(created.public_id, uuid.encode(created.id))
+  t.is(created.user_id, image.user_id)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
+})
+
+test('like image', async t => {
+  let db = t.context.db
+  t.is(typeof db.likeImage, 'function', 'likeImage is a function')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.likeImage(created.public_id)
+
+  t.is(result.likes, image.likes + 1)
+  t.truthy(result.liked)
+})
+
+test('get image', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getImage, 'function', 'getImage is a function')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.getImage(created.public_id)
+
+  t.deepEqual(created, result)
+
+  t.throws(db.getImage('foo'), /not found/)
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+  let images = fixtures.getImages(3)
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImages()
+
+  t.is(typeof db.getImages, 'function', 'listImages is a function')
+  t.is(created.length, result.length)
+})
+
+test('save user', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.saveUser, 'function', 'saveUser is a function')
+
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  let created = await db.saveUser(user)
+
+  t.is(user.username, created.username)
+  t.is(user.email, created.email)
+  t.is(user.name, created.name)
+  t.is(utils.encrypt(plainPassword), created.password)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
+})
+
+test('get user', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getUser, 'function', 'getUser is a function')
+
+  let user = fixtures.getUser()
+  let created = await db.saveUser(user)
+  let result = await db.getUser(user.username)
+
+  t.deepEqual(created, result)
+  t.throws(db.getUser('foo'), /not found/)
+})
+
+test('authenticate user', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.authenticate, 'function', 'authenticate is a function')
+
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  await db.saveUser(user)
+
+  let success = await db.authenticate(user.username, plainPassword)
+  t.true(success)
+
+  let fail = await db.authenticate(user.username, 'foo')
+  t.false(fail)
+
+  let failure = await db.authenticate('foo', 'bar')
+  t.false(failure)
+})
